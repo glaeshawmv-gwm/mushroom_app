f2e335f40f930c31b0c0e680d451180898725d61
Glaesha Vinluan <glaeshawmv@gmail.com>
2025-11-26 14:41:28 +0800

    Remove unnecessary files for Railway deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn predict:app --timeout 300 --workers 1 --bind 0.0.0.0:$PORT
--- app.py
+++
@@ -1,8 +0,0 @@
-# app.py
-from train import app
-
-if __name__ == "__main__":
-    # Use PORT from environment variable for Render
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
